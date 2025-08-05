b78adca089bb30b865fe90e5e3e97fbd6da290c7
Erickson <rixon.code@gmail.com>
2025-08-05 19:04:54 +0800

    ADD: create TechStack component to manage technology assets

---
+++ src/components/Data/TechStack.js
@@ -0,0 +1,31 @@
+// techStack.js or directly in your component file
+import tech_1 from '../../assets/tech_1.svg';
+import tech_2 from '../../assets/tech_2.svg';
+import tech_3 from '../../assets/tech_3.svg';
+import tech_4 from '../../assets/tech_4.svg';
+import tech_5 from '../../assets/tech_5.svg';
+import tech_6 from '../../assets/tech_6.svg';
+import tech_7 from '../../assets/tech_7.svg';
+import tech_8 from '../../assets/tech_8.svg';
+import tech_9 from '../../assets/tech_9.svg';
+import tech_10 from '../../assets/tech_10.svg';
+import tech_11 from '../../assets/tech_11.svg';
+import tech_12 from '../../assets/tech_12.svg';
+import tech_13 from '../../assets/tech_13.svg';
+import tech_14 from '../../assets/tech_14.svg';
+import tech_15 from '../../assets/tech_15.svg';
+import tech_16 from '../../assets/tech_16.svg';
+import tech_17 from '../../assets/tech_17.svg';
+import tech_18 from '../../assets/tech_18.svg';
+import tech_19 from '../../assets/tech_19.svg';
+import tech_20 from '../../assets/tech_20.svg';
+import tech_21 from '../../assets/tech_21.svg';
+import tech_22 from '../../assets/tech_22.svg';
+
+export const techStack = [
+  tech_1, tech_2, tech_3, tech_4, tech_5,
+  tech_16, tech_22, tech_6, tech_17, tech_15,
+  tech_7, tech_8, tech_10, tech_9, tech_11,
+  tech_12, tech_13, tech_14, tech_18, tech_19,
+  tech_20, tech_21
+];
